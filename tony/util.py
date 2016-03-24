8ed0e981ea205720261bd8d81e19b83e2000427b
humanrocker <humanhong1001@gmail.com>
2016-03-24 14:46:36 +0100

    Add string cleaning functions

--- tony/util.py
+++ tony/util.py
@@ -4,9 +4,23 @@
 
 # Verify that an object is iterable if it implemented the iterator protocol
 def isiterable(obj):
-'''This function would return Truefor strings as well as most Python collection types'''
+    '''This function would return Truefor strings as well as most Python collection types'''
     try:
         iter(obj)
         return True
     except TypeError: # not iterable
         return False
+
+# make a list of the operations you want to apply to a particular set of strings:
+def remove_punctuation(value):
+    return re.sub('[!#?]', '', value)
+
+clean_ops = [str.strip, remove_punctuation, str.title]
+
+def clean_strings(strings, ops):
+    result = []
+    for value in strings:
+        for function in ops:
+            value = function(value)
+        result.append(value)
+    return result
