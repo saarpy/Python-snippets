94ddcda538f7c28c8b59da2921b2e177b4ad7644
humanrocker <humanhong1001@gmail.com>
2016-04-10 13:32:59 +0200

    Adjust places of comments

--- tony/pandas.py
+++ tony/pandas.py
@@ -5,21 +5,22 @@
 import numpy as np
 import pandas as pd
 
-
 # TODO: only 2-level, too specific, need extension
-'''
-    Retrieve label1-level vector from 2-level series
-'''
+
 def getVector(series, label):
+    '''
+        Retrieve label1-level vector from 2-level series
+    '''
     try:
         return series.ix[label]
     except KeyError, e:
         return pd.DataFrame().sum()
 
-'''
-    Retrieve label2-value from 2-level series
-'''
+
 def getValue(series, label1, label2):
+    '''
+        Retrieve label2-value from 2-level series
+    '''
     try:
         return series.ix[label].ix[labe2]
     except KeyError, e:
