c138e9e9215da976d19b9ffbb6b79c02e7ef676a
humanrocker <humanhong1001@gmail.com>
2016-04-15 18:27:14 +0200

    Add first impletmentation and tests for n-level Series

--- tony/pandas.py
+++ tony/pandas.py
@@ -21,3 +21,36 @@ def getValueL2(series, label1, label2):
         return series.ix[label1].ix[label2]
     except KeyError, e:
         return 0
+
+
+def getValue(series, *labels):
+    '''Retrieve label-n-value from n-level series.
+    '''
+    result = series
+    i = 0
+    try:
+        for l in labels:
+            indexLambda = getIndexLambda(thisSeries)
+            result = indexLambda(l)
+            i = i + 1
+    except KeyError, e:
+        if i < len(labels):
+            return pd.DataFrame().sum()
+        else:
+            return 0
+
+def getIndexLambda(series):
+    return lambda i: series.ix[i]
+
+
+def main():
+    df = pd.DataFrame({ 'A' : 1.,
+        'B' : pd.Timestamp('20130102'),
+        'C' : pd.Series(1,index=list(range(4)),dtype='float32'),
+        'D' : np.array([3] * 4,dtype='int32'),
+        'E' : pd.Categorical(["test","train","test","train"]),
+        'F' : 'foo' })
+
+
+if __name__ == '__main__':
+    main()
